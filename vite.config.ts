63b21188795c6a2e26d3a2f0579da9ac87ffb877
Antonyes222 <antonspeth07@gmail.com>
2026-06-01 03:10:03 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/miracle_drug_website_writing_50/',
+  plugins: [react(), tailwindcss()],
 })
